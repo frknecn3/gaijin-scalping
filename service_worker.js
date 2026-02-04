316618c47417d566c1f5a278e8c4d21a7880afb0
Furkan Ercan <furkanercan@gmail.com>
2026-02-04 17:06:51 +0300

    fix(folder structure): moved outside parent

--- service_worker.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender) => {
-  if (!msg.inject || !sender.tab?.id) return;
-
-  chrome.scripting.executeScript({
-    target: { tabId: sender.tab.id },
-    world: "MAIN",              // 👈 ASIL OLAY BU
-    files: ["page-intercept.js"]
-  });
-});
