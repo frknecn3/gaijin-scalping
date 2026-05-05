f608506de349a3b30d1b49e6b9e7893d1cc8131d
frknecn3 <furkanduhannuman@gmail.com>
2026-05-06 01:03:06 +0300

    fix(app.ts): remove unnecessary console logs for cleaner output

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,6 @@ dotenv.config();
 // function sleep(ms:number) {
 //     return new Promise(res => setTimeout(res, ms));
 // }
-console.log("SERVERRR")
 
 const app = express();
 
@@ -41,12 +40,10 @@ app.get('/renewOrders', (req, res):void => {
 })
 
 app.get('/orders', async (req, res) => {
-    console.log("istek geldi")
     let items = fs.existsSync('./data/items.json') ? JSON.parse(
         await fs.promises.readFile('./data/items.json', 'utf8')
     ) : []
 
-    console.log("itemlar: ", items)
     if(!items) {
         await fs.promises.writeFile('./data/items.json', JSON.stringify([]))
         items = [];
